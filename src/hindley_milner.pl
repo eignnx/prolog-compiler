8e659e3e25f5708c902ff75604a653ca7cf66804
eignnx <eignnx@gmail.com>
2022-03-10 19:52:04 -0500

    use `=>` instead of `forall`, document types

--- src/hindley_milner.pl
+++ src/hindley_milner.pl
@@ -1,6 +1,28 @@
 :- op(600, yfx, '@').  % Function application
-:- op(650, xfy, '=>'). % Type variable introduction
+:- op(1060, xfy, '=>'). % Type variable introduction
 :- op(1150, fx, mode).
+
+valid_tcx([]).
+valid_tcx([X;Vs=>Ty | Tcx]) :-
+    atom(X),
+    set_of_vars(Vs),
+    valid_tau_type(Ty),
+    valid_tcx(Tcx).
+
+set_of_vars([]).
+set_of_vars([V | Vs]) :-
+    var(V),
+    set_of_vars(Vs),
+    maplist(\==(V), Vs).
+
+valid_tau_type(T) :- atom(T).
+valid_tau_type(A->B) :-
+    valid_tau_type(A),
+    valid_tau_type(B).
+valid_tau_type(pair(A, B)) :-
+    valid_tau_type(A),
+    valid_tau_type(B).
+valid_tau_type(V) :- var(V).
 
 /*
 The `inference` predicate makes a distinction between Inference Variables and
@@ -20,67 +42,68 @@ being a temporary Inference Variable (even though the entire expression ends
 up having type `nat`).
 */
 
-:- mode inference(+_TypeContext, +_Term, -_GenericVariables, -_Type).
+:- mode inference(+_TypeContext, +_Term, -(_GenericVariables=>_Type)).
 
-inference(_Tcx, N, [], nat) :- integer(N), N >= 0.
+inference(_Tcx, N, []=>nat) :- integer(N), N >= 0.
 
-inference(_Tcx, true,  [], bool).
-inference(_Tcx, false, [], bool).
+inference(_Tcx, true,  []=>bool).
+inference(_Tcx, false, []=>bool).
 
-inference(Tcx, pair(A, B), Vs, pair(ATy, BTy)) :-
-    inference(Tcx, A, AVs, ATy),
-    inference(Tcx, B, BVs, BTy),
+inference(Tcx, pair(A, B), Vs=>pair(ATy, BTy)) :-
+    inference(Tcx, A, AVs=>ATy),
+    inference(Tcx, B, BVs=>BTy),
     term_variables(AVs-BVs, Vs). % The poor man's set union.
 
-inference(Tcx, X, Vs, Ty) :-
+inference(Tcx, X, Vs=>Ty) :-
     atom(X),
-    member(X:forall(Vs, Ty), Tcx).
+    member(X;Vs=>Ty, Tcx).
 
-inference(Tcx, X->B, Vs, XTy->BTy) :-
+inference(Tcx, X->B, Vs=>XTy->BTy) :-
     atom(X),
     % Function parameters are not allowed to have generic types. For example
     % this is not allowed:
     % `fn thing(x: for<T> (T, T)) -> int`
-    HypotheticalTcx = [X:forall([], XTy) | Tcx],
-    inference(HypotheticalTcx, B, Vs, BTy).
+    HypotheticalTcx = [X;[]=>XTy | Tcx],
+    inference(HypotheticalTcx, B, Vs=>BTy).
 
-inference(Tcx, let(X, E, B), BVs, BTy) :-
+inference(Tcx, let(X, E, B), BVs=>BTy) :-
     atom(X),
-    inference(Tcx, E, _, ETy),
+    inference(Tcx, E, _=>ETy),
     term_variables(ETy, EVs),
-    inference([X:forall(EVs, ETy) | Tcx], B, BVs, BTy).
+    HypotheticalTcx = [X;EVs=>ETy | Tcx],
+    inference(HypotheticalTcx, B, BVs=>BTy).
 
-inference(Tcx, Fn@Arg, Vs, RetTy) :-
-    inference(Tcx, Arg, ArgVs, ArgTy),
-    inference(Tcx, Fn, FnVs0, FnTy),
+inference(Tcx, Fn@Arg, Vs=>RetTy) :-
+    inference(Tcx, Arg, ArgVs=>ArgTy),
+    inference(Tcx, Fn, FnVs0=>FnTy),
     copy_term(FnVs0-FnTy, FnVs-(ParamTy->RetTy)),
     ParamTy = ArgTy,
     term_variables(FnVs-ArgVs, Vs). % The poor man's set union.
 
 
-:- mode test_case(+_Tcx, +_Tm, +_ExpectedGenericVariables, +_ExpectedType).
+:- mode test_case(+_Tcx, +_Tm, +(_ExpectedGenericVariables=>_ExpectedType)).
 
-test_case([], 123, [], nat).
-test_case([], true, [], bool).
-test_case([], false, [], bool).
-test_case([], pair(123, true), [], pair(nat, bool)).
-test_case([], x->123, [], _T->nat).
-test_case([], x->x, [], T->T).
-test_case([], let(f, x->x, f), [T], T->T).
-test_case([], let(f, x->y->pair(x,y), f), [A, B], A->B->pair(A, B)).
-test_case([], let(f, x->x, f@123), [], nat).
-test_case([], let(f, x->x, pair(f@123, f@true)), [], pair(nat, bool)).
-test_case([], let(id, x->x, let(f, y->id@y, f)), [A], A->A).
-test_case([], let(x, 123, x), [], nat).
-test_case([], let(add, x->y->123, add@123@123), [], nat).
-test_case([x:forall([], nat)], x, [], nat).
+test_case([], 123, []=>nat).
+test_case([], true, []=>bool).
+test_case([], false, []=>bool).
+test_case([], pair(123, true), []=>pair(nat, bool)).
+test_case([], x->123, []=>_T->nat).
+test_case([], x->x, []=>T->T).
+test_case([], let(f, x->x, f), [T]=>T->T).
+test_case([], let(f, x->y->pair(x,y), f), [A, B]=>A->B->pair(A, B)).
+test_case([], let(f, x->x, f@123), []=>nat).
+test_case([], let(f, x->x, pair(f@123, f@true)), []=>pair(nat, bool)).
+test_case([], let(id, x->x, let(f, y->id@y, f)), [A]=>A->A).
+test_case([], let(x, 123, x), []=>nat).
+test_case([], let(add, x->y->123, add@123@123), []=>nat).
+test_case([x;[]=>nat], x, []=>nat).
 
 
 test :-
     forall(
-        test_case(Tcx, Tm, ExpectedVs, ExpectedTy),
+        test_case(Tcx, Tm, ExpectedVs=>ExpectedTy),
         (
-            inference(Tcx, Tm, ActualVs, ActualTy) ->
+            inference(Tcx, Tm, ActualVs=>ActualTy) ->
             (
                 ExpectedVs-ExpectedTy =@= ActualVs-ActualTy -> true
                 ; format('!!! Test Failure:~n  Term: ~p~n  Expected type: ~p=>~p~n  Actual type:   ~p=>~p~n~n', [Tm, ExpectedVs, ExpectedTy, ActualVs, ActualTy])
